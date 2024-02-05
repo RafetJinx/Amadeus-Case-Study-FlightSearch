03f38b47ea8ba97e247af6f11e420f9850bb6958
Rafet Ersoy <63067602+RafetJinx@users.noreply.github.com>
2024-02-06 01:41:12 +0300

    created base CRUD operations for flight models

---
+++ src/main/java/com/example/FlightSearch/service/flight/FlightService.java
@@ -0,0 +1,36 @@
+package com.example.FlightSearch.service.flight;
+
+import com.example.FlightSearch.model.flight.Flight;
+import com.example.FlightSearch.repository.flight.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FlightService {
+
+    private final FlightRepository flightRepository;
+
+    @Autowired
+    public FlightService(FlightRepository flightRepository) {
+        this.flightRepository = flightRepository;
+    }
+
+    public List<Flight> findAllFlights() {
+        return flightRepository.findAll();
+    }
+
+    public Optional<Flight> findFlightById(Long id) {
+        return flightRepository.findById(id);
+    }
+
+    public Flight saveFlight(Flight flight) {
+        return flightRepository.save(flight);
+    }
+
+    public void deleteFlight(Long id) {
+        flightRepository.deleteById(id);
+    }
+}

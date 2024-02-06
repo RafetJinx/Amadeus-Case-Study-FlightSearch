651e6be2989616a74bab2a4b51f42966d0cdeeb6
Rafet Ersoy <63067602+RafetJinx@users.noreply.github.com>
2024-02-06 21:13:27 +0300

    Functions defined for "search api" added to service

--- src/main/java/com/example/FlightSearch/service/flight/FlightDetailsService.java
+++ src/main/java/com/example/FlightSearch/service/flight/FlightDetailsService.java
@@ -3,6 +3,7 @@ package com.example.FlightSearch.service.flight;
 import com.example.FlightSearch.core.utilities.results.dataResult.DataResult;
 import com.example.FlightSearch.model.flight.FlightDetails;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,4 +16,14 @@ public interface FlightDetailsService {
     DataResult<FlightDetails> saveFlightDetails(FlightDetails flightDetails);
 
     DataResult deleteFlightDetails(Long id);
+
+    DataResult<List<FlightDetails>> findFlightsById(Long departureAirportId,
+                                                    Long arrivalAirportId,
+                                                    LocalDateTime departureTime,
+                                                    LocalDateTime returnTime);
+
+    DataResult<List<FlightDetails>> findFlightsByName(String departureAirportName,
+                                                      String arrivalAirportName,
+                                                      LocalDateTime departureTime,
+                                                      LocalDateTime returnTime);
 }

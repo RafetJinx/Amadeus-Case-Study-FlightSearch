0040cc109dd0b635b483020b0daf85eecadc78ae
Rafet Ersoy <63067602+RafetJinx@users.noreply.github.com>
2024-02-06 23:42:55 +0300

    uğdated FlightType

--- src/main/java/com/example/FlightSearch/model/flight/FlightType.java
+++ src/main/java/com/example/FlightSearch/model/flight/FlightType.java
@@ -1,6 +1,24 @@
 package com.example.FlightSearch.model.flight;
 
-public enum FlightType {
-    OUT_BOUND,
-    RETURN
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "FlightType")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class FlightType {
+    @Id
+    private Long id;
+
+    @Column(name = "type")
+    private String type;
 }
